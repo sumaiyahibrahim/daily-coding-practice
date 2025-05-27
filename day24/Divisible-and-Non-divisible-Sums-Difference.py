ff6c0e6b4ca7fcdd85ec364307e3ed9e7f5586d4
Sumaiyah Ibrahim <168560482+sumaiyahibrahim@users.noreply.github.com>
2025-05-27 17:55:06 +0530

    Day 9: Solved Divisible and Non-divisible Sums Difference (Python)

---
+++ day24/Divisible-and-Non-divisible-Sums-Difference.py
@@ -0,0 +1,5 @@
+class Solution(object):
+    def differenceOfSums(self, n, m):
+        totalSum = n * (n + 1) // 2
+        divisibleSum = m * (n // m) * (n // m + 1)
+        return totalSum - divisibleSum
